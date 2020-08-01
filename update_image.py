b33d55ed4ae121b4996e76155176c2e3207a4cc6
vinga2805 <vishalonwork@gmail.com>
2020-08-01 12:58:52 +0530

    adding tekion repo

--- update_image.py
+++ update_image.py
@@ -8,7 +8,7 @@ def update_image_tags(filePath, imageTag):
 
     for _file in filePath:
         # Loading the yaml data from values file
-        subprocess.Popen("cd helm-charts/tekion-app", shell=True).communicate()
+        subprocess.Popen("cd values-files && git checkout master", shell=True).communicate()
         with open(_file, 'r') as yamlFile:
             data = yaml.load(yamlFile, Loader=yaml.FullLoader)
 
@@ -20,7 +20,7 @@ def update_image_tags(filePath, imageTag):
             with open(_file, "w") as yamlFile:
                 yamlFile.write(yaml.dump(data, sort_keys=True))
             print("Bumping up the chart version!!")
-            os.system('pybump bump --file helm-charts/tekion-app/Chart.yaml --level patch')
+            os.system('pybump bump --file values-files/helm-charts/tekion-app/Chart.yaml --level patch')
             #pushToBitBucket()
             print ("Updated the image")
             return
@@ -29,6 +29,7 @@ def pushToBitBucket():
     # repo       = Repo('values-files')
     try:
         commands = """
+            cd values-files && \
             git status && \
             git branch && \
             git diff && \
@@ -36,7 +37,7 @@ def pushToBitBucket():
             git config --global user.email 'you@example.com' && \
             git config --global user.name 'jenkins@zeta.tech' && \
             git commit -m 'Commit from JenkinsBot to update ValuesFile' && \
-            git push
+            git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/tekion.git
             """
         process = subprocess.Popen(
             commands,
@@ -60,6 +61,6 @@ if __name__ == "__main__":
 
     imageTag         = sys.argv[1].split(':')[1]
 
-    filePath = [ "helm-charts/tekion-app/values.yaml"]
+    filePath = [ "values-files/helm-charts/tekion-app/values.yaml"]
 
     update_image_tags(filePath, imageTag)
